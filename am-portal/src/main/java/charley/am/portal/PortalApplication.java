e9ce8481ca8ad17b9fe4d2b4b0ac98fda1c54402
CharleyWuCL <charleywucl@gmail.com>
2018-10-18 09:09:00 +0800

    feat: 添加Portal页面

---
+++ am-portal/src/main/java/charley/am/portal/PortalApplication.java
@@ -0,0 +1,17 @@
+package charley.am.portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 功能描述
+ *
+ * @author Charley
+ * @since 1.0
+ */
+@SpringBootApplication
+public class PortalApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(PortalApplication.class, args);
+  }
+}
